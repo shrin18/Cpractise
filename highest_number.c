c8c90ae977a66dc34532c64e088756c4eec8cd82
Shrinish Donde <shdonde@gmail.com>
2020-09-05 12:55:42 +0200

    Create highest_number.c

---
+++ highest_number.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, num3;
+    int max,min;
+
+    /* Input two numbers from user */
+    printf("Enter three numbers to find maximum: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+
+    if(num1>num2)
+    {
+      if(num1>num3)
+      {
+        max = num1;
+      }
+      else{
+        max = num3;
+      }
+    }
+    else{
+      if (num2>num3)
+      {
+        max = num2;
+      }
+      else {
+        max = num3;
+      }
+    }
+    printf("Highest number is %d", max);
+    return 0;
+}
