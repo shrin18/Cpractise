3fc7fed8c0ea423335e20faeb3b8f5fcc6bfd454
Shrinish Donde <shdonde@gmail.com>
2020-09-06 17:07:48 +0200

    Update linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -11,26 +11,24 @@ node * create(int n){
     node * temp = NULL;
     node * p = NULL;
  
-  for(int i = 0; i < n ; i++){
-  
-  //create individual note
-  temp = (node*)malloc(sizeof(node));
-  printf("\nNode created, please enter  value for the node: %d ", (i+1));
-  scanf("%d",&(temp->value));
-  temp->next=NULL;
+  for(int i=0; i<n; i++)
+  {
+      temp = (node*)malloc(sizeof(node));
+      printf("\n Enter value for the node %d: ", (i+1));
+      scanf("%d",&(temp->value));
+      temp->next=NULL;
   
     if(head == NULL)
       {
         head = temp;
       }
         else{
-          p = head;
-            while(p->next!= NULL)
-            p = p->next;
-            p->next = temp;
+              p = head;
+                while(p->next!= NULL)
+                p = p->next;
+                p->next = temp;
             }   
       }
- 
  return head;
 }
 
@@ -47,9 +45,9 @@ void display(node * head){
 int main(){
   int n=0;
   node * head = NULL;
-  printf("how many nodes do you want to create?");
+  printf("How many nodes do you want to create?");
   scanf("%d",&n);
-  
+
   head = create(n);
   display(head);
   
