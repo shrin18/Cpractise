131210700f40d38415d9958a70d5501a5dd64e8d
Shrinish Donde <shrinish@kth.se>
2020-10-27 10:33:14 +0100

    refactor

--- advanced_ll.c
+++ advanced_ll.c
@@ -9,19 +9,22 @@ typedef struct node {
 void print_list(node_t *head)
 {
   node_t *temp = head;
+
   while(temp!=NULL)
   {
-    printf("%d -", temp->value);
+    printf("%d-", temp->value);
     temp = temp->next;
   }
   printf("\n");
 }
 
-void *node_create(int value)
+void *create_node(int value)
 {
   node_t *result = malloc(sizeof(node_t));
+
   result->value = value;
   result->next = NULL;
+
   return result;
 }
 
@@ -31,42 +34,43 @@ node_t* insert_head(node_t **head, node_t *node_insert)
   *head = node_insert;
   return node_insert;
 }
-
-node_t* find_list(node_t *head, int value)
+  
+node_t *find_list(node_t *head, int value)
 {
   node_t *temp = head;
-  while (temp != NULL)
+  while (temp!=NULL)
   {
     if(temp->value == value)
     {
       return temp;
       temp = temp->next;
     }
-    
   }
   return NULL;
 }
 
-void *insert_node(node_t *node_after, node_t *newnode )
+void *insert_node(node_t *after_node, node_t *newnode)
 {
-  node_after->next = newnode;
-  newnode->next = node_after->next;
+  after_node->next = newnode;
+  newnode->next = after_node->next;
 }
 
-int main(void) {
+int main()
+{
   node_t *n1;
   n1 = malloc(sizeof(node_t));
-  node_t *head = NULL, *temp;
 
-  for (int i=0; i<15; i++)
+  node_t *head = NULL,*temp;
+
+  for(int i=0; i<5; i++)
   {
-  temp = node_create(i);
-  //temp->next = head;
-  head = insert_head(&head,temp);
+    temp = create_node(i);
+    head = insert_head(&head, temp);
   }
-  temp = find_list(head, 13);
-  printf("found node with value %d\n", temp->value );//newnode = temp
-  insert_node(temp,node_create(75));
+  temp = find_list(head,13);
+
+  printf("found node %d\n", temp->value);
+  insert_node(temp,create_node(75));
   print_list(head);
 
   return 0;
