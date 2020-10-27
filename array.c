2d9526a94623a9a091ca97c5723d69833eb6d99a
Shrinish Donde <shrinish@kth.se>
2020-10-27 10:55:09 +0100

    refactor

--- array.c
+++ array.c
@@ -1,37 +1,31 @@
 #include <stdio.h>
-#include <limits.h>
+#define MAX 100
 
-int main(void) {
-  
-  int arr[4] = {1,4,14,15};
-  int size = 4;
-  int sum = 0;
-  
-  int n;
-  int count = 0;
+int main()
+{
+    int arr[MAX],n,i;
+    int num,count;
+     
+    printf("Enter total number of elements: ");
+    scanf("%d",&n);
 
-  printf("Enter the integer whose occurences is required  ");
-  scanf("%d", &n);
-
- 
-  while (n != 0) {
-        n /= 10;     // n = n/10
-        ++count;
+    printf("Enter array elements:\n");
+    for(i=0;i< n;i++)
+    {
+        printf("Enter element %d: ",i+1);
+        scanf("%d",&arr[i]);
     }
-  
-  printf("\n Number of digits: %d\n", count);
-
-  for (int j=size-1; j>=0; j--)
-  {
-    printf("%d \t", arr[j]);
-  }
-
-  for (int i=0; i<size; i++)
-  {
-    sum = sum + arr[i];
-  }
-  printf("\n Sum of array is %d", sum);
-
-
-  return 0;
+     
+    printf("Enter number to find Occurrence: ");
+    scanf("%d",&num);
+     
+    //count occurance of num
+    count=0;
+    for(i=0;i< n;i++)
+    {
+        if(arr[i]==num)
+            count++;
+    }
+    printf("Occurrence of %d is: %d\n",num,count);
+    return 0;
 }
