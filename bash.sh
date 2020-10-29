6b0272283d6327f6812e8e070ce14255817f573d
Shrinish Donde <shrinish@kth.se>
2020-10-29 19:25:17 +0100

    add basic commands

--- bash.sh
+++ bash.sh
@@ -12,7 +12,34 @@ fi
 FILE="test.txt"
 if [ -f "$FILE" ]
 then
-    echo "$FILE is a file"
+    echo "$FILE exists"
 else 
-    echo "$FILE is not a file"
+    echo "$FILE does not exist"
 fi  
+
+read -p  "Are you a member ?" ANSWER
+case "$ANSWER" in
+    [yY] | [yY][eE][sS])
+        echo "You are a member"
+        ;;
+    [nN] | [nN][o0])
+        echo "You are not a memebr"
+        ;;
+    *)
+        echo "PLese anter y or n"
+        ;;
+    esac
+
+NAMES="Shrinish Vedu"
+for NAME in $NAMES
+    do 
+        echo "Hello $NAME"
+done
+
+FILES=$(ls *.txt)
+NEW="new"
+for FILE in $FILES
+    do 
+        echo "renaming the file $FILE to new-$FILE"
+        mv $FILE $NEW-$FILE
+done
